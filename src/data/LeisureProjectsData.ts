9774fbe57edda75bac8b76f6c02577b4a592f328
BuddhikaRJ <buddhikarj@gmail.com>
2022-11-13 12:00:13 +0530

    ar game details added, deployed

--- src/data/LeisureProjectsData.ts
+++ src/data/LeisureProjectsData.ts
@@ -22,18 +22,39 @@ export default [
     </div>
 
     <div class="paragraph center">
-    <img class="phone-screenshot" src="img/projects/screenshots/hiveMenu.jpg" alt="crazy space game screenshot" />
-    <img class="phone-screenshot" src="img/projects/screenshots/hiveShips.jpg" alt="crazy space game screenshot" />
-    <img class="phone-screenshot" src="img/projects/screenshots/hivePlanets.jpg" alt="crazy space game screenshot" />
-    <img class="phone-screenshot" src="img/projects/screenshots/hiveGameplay.jpg" alt="crazy space game screenshot" />
-    <img class="phone-screenshot" src="img/projects/screenshots/hiveGameover.jpg" alt="crazy space game screenshot" />
+    <img class="phone-screenshot" src="img/projects/screenshots/hiveMenu.jpg" alt="Hive game screenshot" />
+    <img class="phone-screenshot" src="img/projects/screenshots/hiveShips.jpg" alt="Hive game screenshot" />
+    <img class="phone-screenshot" src="img/projects/screenshots/hivePlanets.jpg" alt="Hive game screenshot" />
+    <img class="phone-screenshot" src="img/projects/screenshots/hiveGameplay.jpg" alt="Hive game screenshot" />
+    <img class="phone-screenshot" src="img/projects/screenshots/hiveGameover.jpg" alt="Hive game screenshot" />
     </div>
 
     
 
     `, "#f72f21", false, false),
 
-    new ProjectData("project-19", "Tutorials", "img/projects/other/tutorials.jpg", 
+    new ProjectData("project-20", "Unity and Vuforia mobile space adventure (prototype)", "img/projects/screenshots/ar_1.jpeg", 
+    `<div class="paragraph">
+     Made in 2015 for a game dev comppetition. This prototype shows how a mobile game can include different commercial brands as in-game resources and powerups.
+     <br>Developed using Unity and Vuforia.
+     <br>All assets were picked from Unity Asset store targeting mobile device performance.
+     <br>Includes in-game portals where the game transitions to the AR camera. Players can scan real world products there.
+     <br>Prototype lets the player find health orbs by scanning a real world product label.
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/gHjg4eiF9Pg" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph center">
+    <img class="large-screenshot" src="img/projects/screenshots/ar_1.jpeg" alt="AR game screenshot" />
+    <img class="large-screenshot" src="img/projects/screenshots/ar_2.jpeg" alt="AR game screenshot" />
+    <img class="large-screenshot" src="img/projects/screenshots/ar_3.jpeg" alt="AR game screenshot" />
+    <img class="large-screenshot" src="img/projects/screenshots/ar_4.jpeg" alt="AR game screenshot" />
+    </div>
+
+    `, "#236d94", false, false),
+
+    new ProjectData("project-21", "Tutorials", "img/projects/other/tutorials.jpg", 
     `<div class="paragraph">
      I enjoy playing games in console, and pay attention to special little details the devs add in them. I often use my free time to improve my Unity skills by experimenting new thisng I see in games. 
      <br>I was able to brush up my knowledge on using new tools such as pro builder in Unity engine thanks to these self studies.
@@ -49,7 +70,7 @@ export default [
 
     `, "#5674bc", false, false),
 
-    new ProjectData("project-20", "Write ups", "img/projects/other/blogs.jpg", 
+    new ProjectData("project-22", "Write ups", "img/projects/other/blogs.jpg", 
     `<div class="paragraph">
      This is a small Write-up that I completed While learning how to make Bitmap Fonts.
     </div>
